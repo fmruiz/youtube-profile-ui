5e46217e58c539e3b1c2d10aaac0553984cae207
FrancoMRuiz <x.francoruiz@gmail.com>
2021-08-09 00:01:59 -0300

    add Banner

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { ThemeProvider } from 'styled-components'
 import { theme } from './theme'
 // components
 import { Header } from './components/Header/Header'
+import { Banner } from './components/Banner/Banner'
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Header />
+      <Banner />
     </ThemeProvider>
   )
 }
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,5 @@
+import React from "react";
+import banner from "../../assets/banner.png";
+import { ImgBanner } from "./styles";
+
+export const Banner = () => <ImgBanner src={banner} alt="banner" />;
