7e38ab73574db49d0ddd6677ab324414654c53e3
FrancoMRuiz <x.francoruiz@gmail.com>
2021-08-25 02:58:03 -0300

    styling scrollbar

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -7,6 +7,23 @@ export const MainContainer = styled.div`
   padding-left: 16px;
   padding-top: 20px;
   overflow-y: scroll;
+
+  ::-webkit-scrollbar-track {
+    background-color: ${p => p.theme.color.gray5};;
+    border-radius: 20px;
+    padding-top: 20px;
+  }
+
+  ::-webkit-scrollbar {
+    background-color: ${p => p.theme.color.gray5};;
+    width: 10px;
+    border-radius: 20px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${p => p.theme.color.gray3};
+    border-radius: 20px;
+  }
 `;
 
 export const Container = styled.div`
