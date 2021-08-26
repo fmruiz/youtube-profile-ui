db3845ab4edeb2de31dddc769c6a336dd551fd3f
FrancoMRuiz <x.francoruiz@gmail.com>
2021-08-26 01:24:51 -0300

    add styling in MenuItem

--- src/components/LeftMenuDesktop/MenuItem/styles.js
+++ src/components/LeftMenuDesktop/MenuItem/styles.js
@@ -2,11 +2,25 @@ import styled from "styled-components";
 
 export const MenuItemContainer = styled.div`
   display: flex;
+  align-items: center;
+  color: white;
+  height: 40px;
+
+  :hover {
+      background-color: ${p => p.theme.color.gray4};
+  }
 `;
 
-export const MenuItemLogo = styled.div``;
+export const MenuItemLogo = styled.div`
+  width: 26px;
+  font-size: 20px;
+  margin-left: 24px;
+  margin-right: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const MenuItemText = styled.div`
-  color: white;
   font-size: ${(p) => p.theme.font.sm2};
 `;
